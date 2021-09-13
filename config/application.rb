6b2f3bf8b69bb504ba6535ac7efa4ce0619e8061
Dieter Lunn <coder2000@gmail.com>
2021-09-12 21:20:04 -0500

    refactor: update app configuration

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -31,8 +31,5 @@ module Spelunk
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
   end
 end
